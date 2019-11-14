fa93e0afa7bff97f37eb287bf3f41eabf166c29f
Dmitrii_Korolev <korwork87@yahoo.com>
2019-11-14 13:05:43 -0500

    first example of creating bucket, put into S3, read buckets from S3 and delete it from S3

---
+++ src/main/java/S3BucketOps.java
@@ -0,0 +1,39 @@
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
+
+public class S3BucketOps {
+
+    public static void main(String[] args) {
+
+
+        Region region = Region.US_EAST_2;
+        S3Client s3 = S3Client.builder().region(region).build();
+        String bucket = "bucket" + System.currentTimeMillis();
+        System.out.println(bucket);
+
+        // Create bucket
+        CreateBucketRequest createBucketRequest = CreateBucketRequest
+                .builder()
+                .bucket(bucket)
+                .createBucketConfiguration(CreateBucketConfiguration.builder()
+                        .locationConstraint(region.id())
+                        .build())
+                .build();
+        s3.createBucket(createBucketRequest);
+        // snippet-end:[s3.java2.s3_bucket_ops.create_bucket]
+
+        // snippet-start:[s3.java2.s3_bucket_ops.list_bucket]
+        // List buckets
+        ListBucketsRequest listBucketsRequest = ListBucketsRequest.builder().build();
+        ListBucketsResponse listBucketsResponse = s3.listBuckets(listBucketsRequest);
+        listBucketsResponse.buckets().forEach(x -> System.out.println(x.name()));
+        // snippet-end:[s3.java2.s3_bucket_ops.list_bucket]
+
+        // Delete empty bucket
+        // snippet-start:[s3.java2.s3_bucket_ops.delete_bucket]
+        DeleteBucketRequest deleteBucketRequest = DeleteBucketRequest.builder().bucket(bucket).build();
+        s3.deleteBucket(deleteBucketRequest);
+        // snippet-end:[s3.java2.s3_bucket_ops.delete_bucket]
+    }
+}
